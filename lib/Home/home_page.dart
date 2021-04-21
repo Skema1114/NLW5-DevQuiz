d2f5f8ca9c4127d8d0724e5ddd24b16227643389
Rafael Aranda Martins <skema1114@hotmail.com>
2021-04-21 06:14:31 -0300

    Iniciando a NLW#5 e finalizando a primeira aula

---
+++ lib/Home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:nlw5_devquiz/Home/widgets/appbar/app_bar_widgey.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
---
+++ lib/core/app_widget.dart
@@ -0,0 +1,14 @@
+//import 'package:nlw5_devquiz/home/home_page.dart';
+//import 'package:nlw5_devquiz/splash/splash_page.dart';
+import 'package:nlw5_devquiz/Home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
